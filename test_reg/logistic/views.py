5aad12a51a987c988b499cb031c979c834c97ac9
andrei <freemail_2019@mail.ru>
2024-02-29 19:20:09 +0800

    Initial commit

---
+++ test_reg/logistic/views.py
@@ -0,0 +1,23 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.viewsets import ModelViewSet
+
+
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_ields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['positions__product__id', ]
+    search_fields = ['positions__product__title', ]
+    ordering_fields = '__all__'
